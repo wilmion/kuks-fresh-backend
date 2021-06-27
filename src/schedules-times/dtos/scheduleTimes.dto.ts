e552ecfe61cca92fcc1eb9f001d30258dee7c125
Wilbert Navarrete <wilmion92@gmail.com>
2021-06-27 05:23:57 +0200

    finish entities and dtos

---
+++ src/schedules-times/dtos/scheduleTimes.dto.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { DaysT } from '../entities/schedule-times.entity';
+
+export class CreateScheduleTimeDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly day: DaysT;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly from: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly to: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly deliveryOff: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly hourlyRate: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly repeatWeekly: number;
+}
+
+export class UpdateAuthDto extends PartialType(CreateScheduleTimeDto) {}
---
+++ src/schedules-times/entities/schedule-times.entity.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type DaysT =
+  | 'Sunday'
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thrusday'
+  | 'Friday'
+  | 'Saturday';
+
+@Schema()
+export class ScheduleTimes extends Document {
+  @Prop({ required: true, type: String, unique: true })
+  readonly day: DaysT;
+
+  @Prop({ required: true, type: Number })
+  readonly from: number;
+
+  @Prop({ required: true, type: Number })
+  readonly to: number;
+
+  @Prop({ required: true, type: Number })
+  readonly deliveryOff: number;
+
+  @Prop({ required: true, type: Number })
+  readonly hourlyRate: number;
+
+  @Prop({ required: true, type: Number })
+  readonly repeatWeekly: number;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(ScheduleTimes);
